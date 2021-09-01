fef2033e03367efad4830f6101be8095e6a858b8
小玉 優人 <usr0103720@gmo.local>
2021-09-01 12:28:19 +0900

    修正

--- test3/index.js
+++ test3/index.js
@@ -1,8 +1,42 @@
-function main() {
+//  font-familyを求める関数
+function fontFamilyFunc() {
     let table = document.querySelector('table');
-    let style = window.getComputedStyle(table)
-    let fontFamily = style.getPropertyValue("font-family")
-    if (fontFamily == "Noto Sans JP")
+    let style = window.getComputedStyle(table);
+    let fontFamily = style.getPropertyValue('font-family');
+
+    return fontFamily;
+}
+
+//  font-sizeを求める関数
+function fontSizeFunc() {
+    let table = document.querySelector('table');
+    let style = window.getComputedStyle(table).fontSize;
+    let stFontSize = parseFloat(style);
+
+    // console.log(stFontSize);
+
+    return stFontSize;
+}
+
+//  フォントファミリーによってフォントサイズを変更する関数
+function changeFontSize(fontFamily) {
+    if (fontFamily == 'Caveat, Hannari') {
+        document.querySelector('*').style.fontSize = '24px';
+        document.querySelector('table input').style.fontSize = '24px';
+    }
+    if (fontFamily == '"Noto Sans JP", sans-serif') {
+        document.querySelector('*').style.fontSize = '16px';
+        document.querySelector('table input').style.fontSize = '16px';
+    }
+}
+
+function main() {
+    let fontFamily = fontFamilyFunc();
+
+    let fontSize = fontSizeFunc();
+
+    //  フォントファミリーによってフォントサイズを変更する関数
+    changeFontSize(fontFamily);
 }
 
 main();
