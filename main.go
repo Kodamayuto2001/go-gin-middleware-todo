dd35ff21d70c2024a8f25915c1f0aeee768e19f0
kodamayuto <yuto2001gyokugan@gmail.com>
2021-09-03 09:19:01 +0900

    DBの処理を追加

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	// "database/sql"
-	// "github.com/joho/godotenv"
-	// "os"
-
+	"database/sql"
+	"github.com/joho/godotenv"
+	"os"
+	"log"
+	// "fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+
+func httpServer(){
 	engine := gin.Default()
 	engine.Static("css","src/css")
 	engine.Static("js","src/js")
@@ -23,8 +25,36 @@ func main() {
 	})
 
 	engine.GET("/signup",func(c *gin.Context){
-		c.HTML(http.StatusOK,"signup.html",gin.H{})
+		c.HTML(http.StatusOK,"signup.html",gin.H{
+			//	サインアップする際はdbにアクセスする必要がある
+			//	html側で{{.message}}で取得したものを表示可能
+			"message" : mysqlQuery(),
+		})
 	})
 
 	engine.Run(":3000")
+}
+
+func mysqlQuery() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db,err := sql.Open("mysql",os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@/"+os.Getenv("DB_NAME"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	id := 1
+	var name string
+	err = db.QueryRow("SELECT name FROM users WHERE id = ?",id).Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return name 
+}
+
+func main() {
+	httpServer()
 }
